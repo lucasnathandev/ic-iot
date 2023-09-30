6a0b011c8109a455c3b216c13d3a04be6b30116a
Lucas Solium Dev <lucas.develop.prog@gmail.com>
2023-09-30 17:04:15 -0300

    feat: Vue and Vuetify initial settings created

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { createApp } from "vue";
+import "./style.css";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+import App from "./App.vue";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+const app = createApp(App);
+app.use(vuetify);
+
+app.mount("#app");
