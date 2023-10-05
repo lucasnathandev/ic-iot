dc51c6789b8d11da1d78c9d4fd58308859482466
Lucas Solium Dev <lucas.develop.prog@gmail.com>
2023-10-04 23:02:02 -0300

    refactor: infraestructure settings updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {},
   plugins: [vue()],
-})
+});
