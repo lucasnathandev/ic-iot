7405f1b44a2637f4c9d80566cbff574fe109087e
Solium Dev <lucas.develop.prog@gmail.com>
2023-11-15 17:22:18 -0300

    CreateAccountView created and DashBoardView updated

--- src/views/DashBoardView.vue
+++ src/views/DashBoardView.vue
@@ -1,26 +1,13 @@
 <script lang="ts" setup>
-import { useRouter } from 'vue-router';
 import DashBoardBoxes from '../components/DashBoardBoxes.vue';
-import { useLoginStore } from '../stores';
+import LogoutNavBar from '../components/LogoutNavBar.vue';
 
-
-const store = useLoginStore()
-
-
-
-function logout() {
-  store.logout()
-
-  useRouter().push('/')
-}
 
 </script>
 
 <template>
-  <nav class="column q-py-md q-px-md items-end">
-    <q-btn class="bg-deep-orange-6 text-white" label="Logout" @click="logout"></q-btn>
-  </nav>
-  <DashBoardBoxes></DashBoardBoxes>
+  <LogoutNavBar />
+  <DashBoardBoxes />
 </template>
 
 
